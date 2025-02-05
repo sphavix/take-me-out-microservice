a45bbeff4c0a65811b1a6fc893fed83518dc3482
Siphamandla Zolakhe <zwelakhespha@gmail.com>
2025-02-05 19:02:33 +0200

    Additional Code

---
+++ Dinner.Api/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using Dinner.Api.Models;
+using Dinner.Api.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dinner.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController(ApplicationDbContext context) : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ApplicationUser>>> GetUsers()
+        {
+            var users = await context.Users.ToListAsync();
+
+            return Ok(users);
+        }
+
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<ApplicationUser>>GetUser(int id)
+        {
+            var user = await context.Users.FindAsync(id);
+            if(user is null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public ActionResult<ApplicationUser> CreateUser(ApplicationUser user)
+        {
+            throw new Exception();
+        }
+    }
+}
