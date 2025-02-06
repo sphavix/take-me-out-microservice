a0a2360b82227fd446dad862a6df9089354416ba
Siphamandla Zolakhe <zwelakhespha@gmail.com>
2025-02-06 06:14:52 +0200

    Implemented CORS

--- Dinner.Api/Program.cs
+++ Dinner.Api/Program.cs
@@ -12,10 +12,15 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 
